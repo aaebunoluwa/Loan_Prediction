72a03eb3a043e4570daff5e65a847206d5a22a09
aaebunoluwa <aaebunoluwa@gmail.com>
2020-12-31 00:36:43 -0800

    modified server.py, app.js, app.html

--- server/server.py
+++ server/server.py
@@ -49,6 +49,9 @@ def predict_approval():
     Monthly_loan_payment = float(request.form['Monthly_Loan_Payment'])
     Monthly_Balance = float(request.form['Monthly_Balance'])
     
+    result = loan_approval(Credit_History,	Total_income, Monthly_loan_payment,	Monthly_Balance)
+    
+            
     response = jsonify({
         'model_prediction': loan_approval(Credit_History,	Total_income, Monthly_loan_payment,	Monthly_Balance)
         })
