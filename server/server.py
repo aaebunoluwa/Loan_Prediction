9883f5f6048012757b906f47d32b26495250bdbe
aaebunoluwa <aaebunoluwa@gmail.com>
2020-12-30 23:21:53 -0800

    modified server.py

--- server/server.py
+++ server/server.py
@@ -6,7 +6,34 @@ Created on Tue Dec 29 09:43:51 2020
 """
 
 from flask import Flask, request, jsonify, render_template
-import util
+#import util
+
+import json
+import pickle
+import numpy as np
+
+__model = None
+__data_columns = None
+
+
+def loan_approval(Credit_History,	Total_income,	Monthly_loan_payment,	Monthly_Balance):
+    load_artifacts()
+    X = np.array([Credit_History,	Total_income,	Monthly_loan_payment,	Monthly_Balance])
+    return int(__model.predict([X])[0])
+    
+
+def load_artifacts():
+    
+    global __model, __data_columns
+    
+    
+    with open('./artifacts/Loan_prediction_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+        
+        
+    with open('./artifacts/data_columns.json', 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+    
 
 app = Flask(__name__, static_url_path="/client", static_folder='../client', template_folder="../client")
 
@@ -23,7 +50,7 @@ def predict_approval():
     Monthly_Balance = float(request.form['Monthly_Balance'])
     
     response = jsonify({
-        'model_prediction': util.loan_approval(Credit_History,	Total_income, Monthly_loan_payment,	Monthly_Balance)
+        'model_prediction': loan_approval(Credit_History,	Total_income, Monthly_loan_payment,	Monthly_Balance)
         })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
